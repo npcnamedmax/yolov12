23badd6559a8c526d31a8316a8f19989b4d1eadd
jx <122459422+npcnamedmax@users.noreply.github.com>
2026-01-12 18:01:52 +0800

    changed mass hyp

--- train.py
+++ train.py
@@ -1,11 +1,10 @@
 from ultralytics import YOLO
 
-model = YOLO('yolov12s.yaml', task='detect')  
+model = YOLO('yolov12m.yaml', task='detect')  
 
-model = model.load("YOLOv12S.pt")
-model.summary()
+model = model.load("yolov12m.pt")
 # Train the model
-model.train(data='data.yaml', task='detect', epochs=150, imgsz=640, patience=30, batch=8, device="cuda:0")
+model.train(data='..data.yaml', task='detect', epochs=150, imgsz=640, patience=30, batch=8, device="cuda:0")
 
 # Evaluate the model on the test set
 # model.val(data='data.yaml', imgsz=640, device="cuda:0", split='val')
